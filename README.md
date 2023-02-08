31ce60e87d68fbfbe0a872c6b9ccc6b556ce96f5
flonix8 <flstanek@googlemail.com>
2023-02-08 17:11:24 +0100

    First working example of a h264 local file play pipeline

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gstreamer-onnx
+
+## Successful GStreamer pipelines
+- Play supplied .mp4 file
+  `gst-launch-1.0 filesrc location=traffic_video.mp4 ! decodebin ! xvimagesink`
+  `gst-launch-1.0 filesrc location=traffic_video.mp4 ! qtdemux ! h264parse ! avdec_h264 ! xvimagesink`
+
+- Play webcam
+  `gst-launch-1.0 v4l2src ! videoconvert ! xvimagesink`
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,66 @@ fn tutorial_main() {
     gstreamer::init().unwrap();
 
     // Build the pipeline
-    let path =
-        "./traffic_video.mp4";
-    let pipeline = gstreamer::parse_launch(&format!("filesrc location={path} ! decodebin ! xvimagesink")).unwrap();
+    let path = "./traffic_video.mp4";
 
+    let filesrc = gstreamer::ElementFactory::make("filesrc")
+        .name("filesrc")
+        .property_from_str("location", path)
+        .build()
+        .expect("Could not create filesrc");
+        
+    let qtdemux = gstreamer::ElementFactory::make("qtdemux")
+        .name("qtdemux")
+        .build()
+        .expect("Could not create qtdemux");
+        
+    let h264parse = gstreamer::ElementFactory::make("h264parse")
+        .name("h264parse")
+        .build()
+        .expect("Could not create h264parse");
+        
+    let avdec_h264 = gstreamer::ElementFactory::make("avdec_h264")
+        .name("avdec_h264")
+        .build()
+        .expect("Could not create avdec_h264");
+
+    let xvimagesink = gstreamer::ElementFactory::make("xvimagesink")
+        .name("xvimagesink")
+        .build()
+        .expect("Could not make xvimagesink");
+
+    let pipeline = gstreamer::Pipeline::builder().name("test-pipeline").build();
+
+    pipeline.add_many(&[&filesrc, &qtdemux, &h264parse, &avdec_h264, &xvimagesink]).unwrap();
+    filesrc.link(&qtdemux).expect("Error linking filesrc to qtdemux");
+    // qtdemux.link(&h264parse).expect("Error linking qtdemux to h264parse");
+    h264parse.link(&avdec_h264).expect("Error linking h264parse to avdec_h264");
+    avdec_h264.link(&xvimagesink).expect("Error linking avdec_h264 to xvimagesink");
+
+    qtdemux.connect_pad_added(move |src, src_pad| {
+        println!("Received new pad {} from {}", src_pad.name(), src.name());
+
+        let sink_pad = h264parse.static_pad("sink").expect("Failed to get static sink pad from h264parse");
+        if sink_pad.is_linked() {
+            println!("We are already linked. Ignoring...");
+            return;
+        }
+
+        let new_pad_caps = src_pad.current_caps().expect("Failed to get caps of new pad");
+        let new_pad_struct = new_pad_caps.structure(0).expect("Failed to get first structure of caps");
+        let new_pad_type = new_pad_struct.name();
+
+        let is_video = new_pad_type.starts_with("video");
+        if is_video {
+            println!("Found first video pad {}. Trying to link.", new_pad_type);
+            let res = src_pad.link(&sink_pad);
+            match res {
+                Ok(_) => println!("Link succeeded."),
+                Err(_) => println!("Link failed."),
+            }
+        }
+    });
+    
     // Start playing
     pipeline
         .set_state(gstreamer::State::Playing)
